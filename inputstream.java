e536feb8e83a2bb71fc2d3303f40b4002acdf90c
creepereye <creepereye12@naver.com>
2020-09-02 23:40:01 +0900

    Add Text File [inputstream.java]

---
+++ inputstream.java
@@ -0,0 +1,29 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class inputstream {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FileInputStream fin=null;
+		InputStreamReader in=null;
+		try {
+			int c;
+			fin=new FileInputStream("C:\\Temp\\test.txt");
+			in=new InputStreamReader(fin,"MS949");
+			System.out.println(in.getEncoding());
+			while ((c=in.read())!=-1) {
+				
+				System.out.print((char)c);
+			}
+			fin.close();
+			in.close();
+		}
+		catch (IOException e) {
+			// TODO: handle exception
+			System.out.print("¿À·ù³µÀ½!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		}
+	}
+
+}
