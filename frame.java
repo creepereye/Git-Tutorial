1ba784f5d024d95bd0fb234f7a598932dca9e5d9
creepereye <creepereye12@naver.com>
2020-09-15 21:33:44 +0900

    [Add Text File frame.java]

---
+++ frame.java
@@ -0,0 +1,20 @@
+
+import javax.swing.JFrame;
+
+class myframe extends JFrame
+{
+	public myframe()
+	{
+		setTitle("실험중 ");
+		setSize(300,300);
+		setVisible(true);
+	}
+}
+public class frame {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		myframe my=new myframe();
+	}
+
+}
