9d96cf91cc44e051057ecef469513fb14026a1eb
creepereye <creepereye12@naver.com>
2020-09-20 21:27:31 +0900

    [Add Text File codetest.java]

---
+++ codetest.java
@@ -0,0 +1,62 @@
+
+package greedy_algorithm;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+class pair implements Comparable<pair>
+{
+	pair(int x,int y)
+	{
+		this.x=x;
+		this.y=y;
+	}
+	int x;
+	int y;
+	@Override
+	public int compareTo(pair o) {
+		if(y!=o.y)
+		{
+			return y-o.y;
+		}
+		else {
+			return x-o.x;
+		}
+		
+	}
+}
+public class test_1931 {
+	static ArrayList<pair> list=new ArrayList<pair>();
+	static int input=0;
+	static int x=0;
+	static int y=0;
+	static int num=0;
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		//��ü ����
+		BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
+		int result=0;
+		input=Integer.parseInt(bf.readLine());
+		for(int i=0;i<input;i++)
+		{
+			StringTokenizer sc=new StringTokenizer(bf.readLine());
+			x=Integer.parseInt(sc.nextToken());
+			y=Integer.parseInt(sc.nextToken());
+			list.add(new pair(x,y));
+		}
+		//��ü ��������
+		Collections.sort(list);
+		int number=-1;
+		for(int i=0;i<list.size();i++)
+		{
+			if(list.get(i).x>=number)
+			{
+				number=list.get(i).y;
+				result++;
+			}
+		}
+		System.out.println(result);
+	}
+}
