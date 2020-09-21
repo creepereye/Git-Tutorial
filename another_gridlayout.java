1e2354118ca7ee98c278d276184c5d4d6b5802de
creepereye <creepereye12@naver.com>
2020-09-21 17:14:07 +0900

    [Add Text File another_gridlayout.java]

---
+++ another_gridlayout.java
@@ -0,0 +1,36 @@
+
+import java.awt.Container;
+import java.awt.GridLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class gridlayout extends JFrame {
+	public gridlayout()
+	{
+		setTitle("gridlayout");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		GridLayout grid=new GridLayout(4,2);
+		grid.setVgap(10);
+		
+		Container con=getContentPane();
+		con.setLayout(grid);
+		
+		con.add(new JLabel("이름"));
+		con.add(new JTextField(""));
+		con.add(new JLabel("학번"));
+		con.add(new JTextField(""));
+		con.add(new JLabel("학과"));
+		con.add(new JTextField(""));
+		con.add(new JLabel("과목"));
+		con.add(new JTextField(""));
+		setSize(300,200);
+		setVisible(true);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new gridlayout();
+	}
+
+}
